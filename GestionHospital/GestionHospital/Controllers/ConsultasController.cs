15be52c43c3f6a2deb8d6d0e974e0f1318f46f9c
ABALCO VALLEJO CARLOS FERNANDO <avcarlos@utpl.edu.ec>
2022-03-11 00:08:31 -0500

    Generación de archivo excel

--- GestionHospital/GestionHospital/Controllers/ConsultasController.cs
+++ GestionHospital/GestionHospital/Controllers/ConsultasController.cs
@@ -1,4 +1,7 @@
-﻿using GestionHospital.Filters;
+﻿using ExportExcel.Code;
+using GestionHospital.Filters;
+using GestionHospital.Logica;
+using GestionHospital.Model.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,18 @@ namespace GestionHospital.Controllers
             ViewBag.Message = "En implementación.";
 
             return View("Index");
+        }
+
+
+        [HttpGet]
+        public FileContentResult ExportToExcel()
+        {
+            AdministracionCore objAdministracion = new AdministracionCore();
+
+            List<Especialidad> especialidades = objAdministracion.ConsultarEspecialidades();
+            string[] columns = { "Nombre", "Descripcion" };
+            byte[] filecontent = ExcelExportHelper.ExportExcel(especialidades, "Especialidades", true, columns);
+            return File(filecontent, ExcelExportHelper.ExcelContentType, "Medicos.xlsx");
         }
     }
 }
